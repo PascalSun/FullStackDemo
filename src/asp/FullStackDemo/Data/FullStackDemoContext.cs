60a2da2b71cfa26de082a396591d27dc10321021
pascalsun <pascalsun23@gmail.com>
2022-01-30 15:19:29 +0800

    add example about how easily to create page in .net

---
+++ src/asp/FullStackDemo/Data/FullStackDemoContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FullStackDemo.Models;
+
+namespace FullStackDemo.Data
+{
+    public class FullStackDemoContext : DbContext
+    {
+        public FullStackDemoContext (DbContextOptions<FullStackDemoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FullStackDemo.Models.Movie> Movie { get; set; }
+    }
+}
--- src/asp/FullStackDemo/Program.cs
+++ src/asp/FullStackDemo/Program.cs
@@ -1,9 +1,23 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using FullStackDemo.Data;
+using FullStackDemo.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<FullStackDemoContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("FullStackDemoContext")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
