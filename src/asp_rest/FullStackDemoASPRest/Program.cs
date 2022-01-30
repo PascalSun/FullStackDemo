4a0d66f29bd38c5fc7e0d3ef5cbd062956e8acac
pascalsun <pascalsun23@gmail.com>
2022-01-30 15:43:27 +0800

    add framework

---
+++ src/asp_rest/FullStackDemoASPRest/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.MapGet(
+    "/", () => "Hello World");
+
+app.Run();
+
+internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
